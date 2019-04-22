8f831a475853d17e90b505e7964e97fd0e9b8cb0
Nina Johnson <9makesthings@gmail.com>
2019-04-22 17:06:01 -0500

    basic homepage setup with buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import './Home.css';
 
+// Material
+import Button from '@material-ui/core/Button';
 
 class Home extends Component {
+
+    handleRoute = (event) => {
+        if( event.target.name === "create-trip") {
+            this.props.history.push('/create-trip');
+        } else {
+            this.props.history.push('/my-trips');
+        }
+    }
     render() {
         return(
             <div>
-                <h2>This is the home page!</h2>
+                <div className="div-left" >
+                    <h2>This is the home page!</h2>
+
+                    <p>This is some intro text...</p>
+
+                    <Button onClick={this.handleRoute} name="create-trip" >Plan a Trip</Button>
+                    <Button onClick={this.handleRoute} name="my-trips" >My Trips</Button>
+                </div>
+                <div className="div-right" >
+                    <img src="https://images.unsplash.com/photo-1510857817970-2a7060a55c8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80" alt="BWCA Image" />
+                </div>
             </div>
         );
     }
