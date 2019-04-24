14e23d3f87d1c2d97efff260ab27f5a1b44eef38
Nina Johnson <9makesthings@gmail.com>
2019-04-24 17:37:10 -0500

    working on adding equipment data to state in StepThree

--- src/components/CreateTrip/StepThree/ChecklistItem.js
+++ src/components/CreateTrip/StepThree/ChecklistItem.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import NextButton from '../NextButton';
+// import NextButton from '../NextButton';
 
 // Material
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
+// import FormControl from '@material-ui/core/FormControl';
+// import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
@@ -15,8 +15,9 @@ class ChecklistItem extends Component {
     state = {
         item: {
             code: '',
-            status: false
-            // trip_id: this.props.reduxState.something!
+            status: false,
+            // trip_id: this.props.reduxState.something? OR 
+                // will this get assigned when data is dispatched to DB?
         }
     }
 
@@ -25,18 +26,25 @@ class ChecklistItem extends Component {
     }
 
     handlechange = (event) => {
-
+        this.setState({
+            item: {
+                code: event.currentTarget.name,
+                status: !this.state.status,
+            }
+        })
+        console.log( `Current ChecklistItem state:`, this.state.item );
+        
     }
 
     render() {
-        const stepAction = {type: 'SET_PACKLIST', payload: this.state};
+        // const stepAction = {type: 'SET_PACKLIST', payload: this.state};
 
         return(
-            <FormControlLabel key={item.id}
+            <FormControlLabel
                 control={
-                    <Checkbox onChange={this.handlechange} value={item.name} />
+                    <Checkbox onChange={this.handlechange} name={this.props.item.code} value={this.props.item.name} />
                 }
-                label={item.name}
+                label={this.props.item.name}
             />
         );
     }
--- src/components/CreateTrip/StepThree/StepThree.js
+++ src/components/CreateTrip/StepThree/StepThree.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import NextButton from '../NextButton';
+// import ChecklistItem from './ChecklistItem';
 
 // Material
 import FormControl from '@material-ui/core/FormControl';
@@ -13,23 +14,48 @@ import Checkbox from '@material-ui/core/Checkbox';
 class StepThree extends Component {
 
     state = {
-        packlist: this.props.reduxState.equipment
     }
 
     componentDidMount() {
         this.props.dispatch( {type: 'GET_EQUIPMENT'} );
     }
 
-    handlechange = (event) => {
+    componentDidUpdate(prevProps) {
+        console.log( this.props.reduxState.equipment, this.state );
+        const equipment = [this.props.reduxState.equipment];
+        console.log( `equipment:`, equipment );
+        
 
-        // if( event.target.key = )
-        // this.setState({
-        //     packlist: [
-        //         ...this.state.packlist,
-        //         event.currentTarget.key: 
-        //     ]
-        // })
+        // if (this.props.reduxState.equipment !== prevProps.reduxState.equipment) {
+
+        //     for( let i=0; i<equipment.length; i++ ){
+        //         this.setState({
+        //             ...this.state,
+        //             [equipment[i].name]: {
+        //                 ...this.state.equipment[i].name,
+        //                 equipment_code: equipment[i].code, 
+        //                 status: false,
+        //             }
+        //         })
+        //     }
+        //     console.log( `componentDidUpdate...`, this.state );
+        // }
     }
+
+    handlechange = (event) => {
+        console.log( `in handleChange...`, this.state );
+        const item = event.currentTarget.name;
+        
+        this.setState({
+            ...this.state,
+            [item]: {
+                ...this.state.item,
+                equipment_code: event.currentTarget.id, 
+                status: true,
+            }
+        })
+    }
+    
 
     render() {
         const stepAction = {type: 'SET_PACKLIST', payload: this.state};
@@ -40,14 +66,14 @@ class StepThree extends Component {
                     <FormGroup>
 
                         {/* This will map and create a checkbox for each item */}
+
                         {this.props.reduxState.equipment.map( item => 
-                                <FormControlLabel key={item.id}
+                                <FormControlLabel key={item.code}
                                     control={
-                                        <Checkbox onChange={this.handlechange} value={item.name} />
+                                        <Checkbox name={item.name} id={item.code} value={this.state.name} onChange={this.handlechange} />
                                     }
                                     label={item.name}
                                 />
-                            
                             )}
 
                     </FormGroup>
